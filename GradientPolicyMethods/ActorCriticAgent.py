fixed REINFORCE with baseline, started to fix vanilla gradient policy

GradientPolicyMethods/ActorCriticAgent.py
@@ -15,9 +15,9 @@ class ActorCriticAgent:
 
         self.function_approximator = None
 
-        self.states = []
-        self.actions = []
-        self.rewards = []
+        self.previous_state = None
+        self.previous_action = None
+        #self.rewards = []
         self.is_continuous = None
 
         self.set_params_from_dict(params)
@@ -72,8 +72,23 @@ class ActorCriticAgent:
         :return:
         """
         self.function_approximator.optimizer.zero_grad()
+        state_value = self.function_approximator.predict(state)
+        prev_state_value = self.function_approximator.predict(self.previous_state)
+        δ = reward + self.discount_factor * state_value.detach() - prev_state_value.detach()
+        value_loss = - prev_state_value * δ
+        value_loss.backward()
+        self.function_approximator.optimizer.step()
+
+        loss = - torch.log(self.policy_estimator.predict(self.previous_state)[self.previous_action]) * δ
+        loss.backward()
+        self.policy_estimator.optimizer.step()
+
+
+        """
+        self.function_approximator.optimizer.zero_grad()
         # computing the advantage.
-        # The advantage is... I need to re-read my RL notes.
+        # The advantage is r + γ * St - St-1
+        # 
         advantage = reward + self.discount_factor * self.function_approximator(torch.FloatTensor(state)) - \
                     self.function_approximator(torch.FloatTensor(self.previous_state))
         #current_state_value = self.function_approximator(torch.FloatTensor(self.previous_state))
@@ -94,7 +109,7 @@ class ActorCriticAgent:
         actor_loss = - torch.log(action_chosen_prob) * advantage.detach()
         actor_loss.backward()
         self.policy_estimator.optimizer.step()
-
+        """
         """
         advantage = reward + self.discount_factor * self.function_approximator(torch.FloatTensor(state)) - \
             self.function_approximator(torch.FloatTensor(self.previous_state))

GradientPolicyMethods/REINFORCEAgent.py
@@ -1,5 +1,7 @@
 from FunctionApproximator import *
 from GradientPolicyMethods.PolicyEstimator import *
+import torch
+
 
 class REINFORCEAgent:
     def __init__(self, params={}):
@@ -15,7 +17,6 @@ class REINFORCEAgent:
         self.is_continuous = None
 
         self.set_params_from_dict(params)
-        # self.set_other_params()
 
     # ====== Initialization functions =======================================================
 
@@ -65,8 +66,6 @@ class REINFORCEAgent:
         # self.states.extend(state)
         # self.episode_memory.extend((reward, state))
 
-
-
     def learn_from_experience(self):
         """ replays the episode backward and make gradient ascent over the policy
         """
@@ -76,7 +75,7 @@ class REINFORCEAgent:
         for reward, state, action in reversed_episode:
             self.policy_estimator.optimizer.zero_grad()
             discounted_reward = reward + self.discount_factor * discounted_reward
-            α γ^t Gt×   
+            # on prend le contraire de l'expression pour que notre loss pénalise au bon moment.
             loss = - torch.log(self.policy_estimator.predict(state)[action]) * discounted_reward
             loss.backward()
             self.policy_estimator.optimizer.step()

GradientPolicyMethods/REINFORCEAgentWithBaseline.py
@@ -107,10 +107,15 @@ class REINFORCEAgentWithBaseline:
 
             self.policy_estimator.optimizer.zero_grad()
             discounted_reward = reward + self.discount_factor * discounted_reward
-            loss = - torch.log(self.policy_estimator.predict(state)[action]) * (discounted_reward - state_value)
+            δ = discounted_reward - state_value.detach()
+            value_loss = - state_value * δ
+            value_loss.backward()
+            self.function_approximator.optimizer.step()
+
+            loss = - torch.log(self.policy_estimator.predict(state)[action]) * δ
             loss.backward()
             self.policy_estimator.optimizer.step()
-            self.function_approximator.optimizer.step()
+            
 
             last_state = state
             last_action = action