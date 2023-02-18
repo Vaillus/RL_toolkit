d350310b034011d5c0e50c0a33314fefe1e62f62
Vaillus <hugovaillaud@gmail.com>
2023-02-18 17:24:58 +0100

    small script for testing

--- test_2.py
+++ test_2.py
@@ -8,33 +8,7 @@ print(state)
 from stable_baselines3 import ppo
 """
 
-import gym
-import wandb
-from wandb.integration.sb3 import WandbCallback
-
-#from stable_baselines3.common import make_vec_env
-#from stable_baselines3.ppo import PPO
-import stable_baselines3 as sb3
-
-config = {
-    "policy_type": "MlpPolicy",
-    "total_timesteps": 250000000,
-    "env_name": "Pendulum-v0",
-}
-run = wandb.init(
-    project="sb3",
-    config=config,
-    sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-    monitor_gym=True,  # auto-upload the videos of agents playing the game
-    save_code=True,  # optional
-)
-
-# multiprocess environment
-#env = make_vec_env('Pendulum-v0', n_envs=4)
-env = gym.make("Pendulum-v1")
-
-model = sb3.ppo.PPO("MlpPolicy", env, verbose=1, tensorboard_log=f"runs/{run.id}", gae_lambda=1.0)
-model.learn(
-    total_timesteps=250000000, 
-    callback=WandbCallback()
-)
+if __name__ =="__main__":
+    for i in range(1000):
+        a = 1+1
+        #print(a)
